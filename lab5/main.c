66adab35412c7f8c49db78cb17b432fe9a56db97
Piotr Pabich <piotrekpabich2@gmail.com>
2023-04-04 17:27:07 +0200

    Created function to display changed number

--- lab5/main.c
+++ lab5/main.c
@@ -32,16 +32,64 @@ long validate_number(const char * text, int * error) {
     return num;
 }
 
+void print_changed_number(long number, int precision) {
+    //printf("%s\n", number);
+    char buff[100] = {0};
+    sprintf(buff, "%ld", number);
+    if(precision != -1) {
+        int spaces = precision - strlen(buff);
+        for(int i = 0; i < spaces; i++) {
+            //printf("i: %d | %d\n", i, precision - strlen(number));
+            putchar(' ');
+        }
+    }
+   // printf("Here\n");
+    for(int i = 0; i < strlen(buff); i++) {
+        if(buff[i] == '-') {
+            putchar(buff[i]);
+            continue;
+        }
+        char num = buff[i] - 1;
+        if(num < '0') {
+            num = '9';
+        }
+        putchar(num);
+    }
+}
+
 int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
-			printf("%s",param);
-		}else
-			putchar(format_string[i]);
-	}
-	puts("");
-	return 0;
+    for(int i=0;i<strlen(format_string);i++){
+        int valid = 0;
+        if(format_string[i] == '#'){
+            const char * tmp = format_string + i + 1;
+            int precision;
+            int res = validate_format(tmp, &precision);
+            int error = 1;
+            if(res != -1) {
+                long number = validate_number(param, &error);
+                if(error == 0) {
+                    if(res == 1) {
+                        print_changed_number(number, -1);
+                    } else {
+                        print_changed_number(number, precision);
+                    }
+                    valid = 1;
+                    for(;;i++) {
+                        char c = format_string[i];
+                        if(c == 'g') {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        // display single char
+        if(!valid) {
+            putchar(format_string[i]);
+        }
+    }
+    puts("");
+    return 0;
 }
 
 int main(int argc, char *argv[]){
