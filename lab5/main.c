efd33ff42a973fab510629e7723d863fdfaedab1
Piotr Pabich <piotrekpabich2@gmail.com>
2023-04-04 17:23:12 +0200

    Created function to validate number

--- lab5/main.c
+++ lab5/main.c
@@ -21,6 +21,17 @@ int validate_format(const char * string, int * precision) {
 
     return -1;
 }
+
+long validate_number(const char * text, int * error) {
+    long num;
+    if(sscanf(text, "%ld", &num) != 1) {
+        *error = 1;
+        return 0;
+    }
+    *error = 0;
+    return num;
+}
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
