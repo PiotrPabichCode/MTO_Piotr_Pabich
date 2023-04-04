7adf61c231b25713519ab0d92eb7e115ff014ca1
Piotr Pabich <piotrekpabich2@gmail.com>
2023-04-04 17:22:33 +0200

    Created function to validate_format

--- lab5/main.c
+++ lab5/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+int validate_format(const char * string, int * precision) {
+    char endpoint;
+    int res = sscanf(string, "%c", &endpoint); // #g
+    if(res == 1) {
+        if(endpoint == '#') {
+            return 3;
+        }
+        if(endpoint == 'g') {
+            return 1;
+        }
+    }
+    res = sscanf(string, "%d%c", precision, &endpoint); // #10g
+    if(res == 2) {
+        if(endpoint == 'g' && *precision >= 0) {
+            return 2;
+        }
+    }
+
+    return -1;
+}
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
