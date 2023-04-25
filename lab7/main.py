1bd4ccbeb970b8354205e9ec5406419047c3a2a1
Piotr Pabich <piotrekpabich2@gmail.com>
2023-04-25 17:26:44 +0200

    Changed my_printf to match requirements

--- lab7/main.py
+++ lab7/main.py
@@ -25,8 +25,8 @@ def my_printf(format_string,param):
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'j':
+                print_changed(param)
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
