06be024df00e7d3c5ce108e9e994e3d286f295bb
Piotr Pabich <piotrekpabich2@gmail.com>
2023-05-16 17:28:31 +0200

    Added function to change printed value

--- lab8/main.py
+++ lab8/main.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+def change_number(number, num_len):
+    hexValue = int(number)
+    strValue = str(hex(hexValue))
+    changed = strValue.replace('0x','')
+    out = []
+    for i in changed:
+        if i == 'a':
+            out.append('g')
+        elif i == 'b':
+            out.append('h')
+        elif i == 'c':
+            out.append('i')
+        elif i == 'd':
+            out.append('j')
+        elif i == 'e':
+            out.append('k')
+        elif i == 'f':
+            out.append('l')
+        elif i == '0':
+            out.append('o')
+        else:
+            out.append(i)
+    leadingZeros = "o" * max(0, int(num_len) - len(out))
+    outNumber = ''.join(str(j) for j in out)
+    output = leadingZeros + outNumber
+    return output
+
 
 def my_printf(format_string,param):
     #print(format_string)
