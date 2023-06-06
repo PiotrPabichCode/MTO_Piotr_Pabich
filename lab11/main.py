9f1d09c789b9ec4a0dee53dfb80e847fe6c37b6d
Piotr Pabich <piotrekpabich2@gmail.com>
2023-06-06 17:20:01 +0200

    Created function to convert_param

--- lab11/main.py
+++ lab11/main.py
@@ -2,6 +2,21 @@
 
 import sys
 
+look_up_table = ['a','b','c','d','e','f','g','h','i','j']
+
+def convert_param(value):
+    output = []
+    value = value[::-1]
+    pos = 0
+    for i in value:
+        if x == '1':
+            output.append(look_up_table[pos%10])
+        else:
+            output.append('0')
+        pos += 1
+    out_s = ''.join(str(i) for i in output)
+    return out_s[::-1]
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
